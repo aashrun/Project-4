948c746e8bd34d75c00384f8be813d64e59138c2
aashrun <aashrungautam15@gmail.com>
2022-07-20 18:34:30 +0530

    haha

--- Controller/getUrl.js
+++ Controller/getUrl.js
@@ -5,10 +5,8 @@ const getUrl = async function (req, res){
         let urlCode = req.params.urlCode
         if(!urlCode) return res.status(400).send({status : false, msg : "Please insert a url code!"})
 
-        if(urlCode.toLowerCase() !== true) return res.status(400).send({status : false, msg : "The Url Code should be in lower case only!"})
-        // let lowercase = /[ A-Za-z]/;
-        // if(!lowercase.test(urlCode))return res.status(400).send({status:false,msg:"The url code should be in lowercase only!"})
-
+        if(urlCode.toLowerCase() !== urlCode) return res.status(400).send({status : false, msg : "The Url Code should be in lower case only!"})
+       
 
         let url= await urlModel.findOne({urlCode : urlCode})
         if(url){
